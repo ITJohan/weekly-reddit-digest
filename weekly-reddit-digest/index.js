9db68c73b5975290f95c479c0747ca428a5964de
Johan Lindkvist <johanlindkvist89@gmail.com>
2021-08-08 15:01:50 +0200

    Refactor to Azure Function

---
+++ weekly-reddit-digest/index.js
@@ -0,0 +1,63 @@
+require('dotenv').config();
+const fetch = require('node-fetch');
+const btoa = require('btoa');
+
+module.exports = async function (context, req) {
+    try {
+        let body = '';
+
+        const response = await fetch('https://www.reddit.com/api/v1/access_token', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                Authorization: `Basic ${btoa(`${process.env.KEY}:${process.env.SECRET}`)}`,
+            },
+            body: `grant_type=password&username=${process.env.USERNAME}&password=${process.env.PASSWORD}&redirect_uri=${process.env.REDIRECT_URI}`,
+        });
+        const data = await response.json();
+
+        console.log(data)
+
+        body += await generateTopic('Science', data.access_token);
+        body += await generateTopic('Games', data.access_token);
+        body += await generateTopic('Technology', data.access_token);
+        body += await generateTopic('Programming', data.access_token);
+        
+        context.res = {
+            'Content-Type': 'text/html',
+            body 
+        };
+    } catch (error) {
+        context.res = {
+            headers: {
+                'Content-Type': 'text/html'
+            },
+            body: `${error}`
+        }       
+    }
+}
+
+const generateTopic = async (subreddit, token) => {
+    let body = '';
+
+    const response = await fetch(`https://oauth.reddit.com/r/${subreddit}/top/?t=week&limit=5`, {
+        headers: {
+            Authorization: `Bearer ${token}`,
+        },
+    });
+    const data = await response.json();
+
+    body += `<h2>${subreddit}</h2>`;
+    body += '<ul>';
+    for (let topic of data.data.children) {
+        console.log(topic);
+        body += `
+            <li><a href="${topic.data.url}" target="_blank">${topic.data.title
+            .slice(0, 40)
+            .concat('...')}</a></li>
+        `;
+    }
+    body += '</ul>';
+
+    return body;
+};
